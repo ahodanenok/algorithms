56ff6c2700b810354cdd71255b827b8df1537fbf
Andrei Hodanenok <xoda91@gmail.com>
2021-06-08 07:11:25 +0300

    book, exercise 1.1.3, add test annotations

--- src/test/java/ahodanenok/algs4/ch_1_1/ThreeEqualTest.java
+++ src/test/java/ahodanenok/algs4/ch_1_1/ThreeEqualTest.java
@@ -6,30 +6,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ThreeEqualTest {
 
+    @Test
     public void testEqual_All() throws Exception {
         StringWriter sw = new StringWriter();
         ThreeEqual.impl(new String[] { "5", "5", "5" }, sw);
         assertEquals("equal", sw.toString());
     }
 
+    @Test
     public void testDifferent_All() throws Exception {
         StringWriter sw = new StringWriter();
         ThreeEqual.impl(new String[] { "1", "2", "3" }, sw);
         assertEquals("not equal", sw.toString());
     }
 
+    @Test
     public void testDifferent_A() throws Exception {
         StringWriter sw = new StringWriter();
         ThreeEqual.impl(new String[] { "2", "4", "4" }, sw);
         assertEquals("not equal", sw.toString());
     }
 
+    @Test
     public void testDifferent_B() throws Exception {
         StringWriter sw = new StringWriter();
         ThreeEqual.impl(new String[] { "7", "5", "7" }, sw);
         assertEquals("not equal", sw.toString());
     }
 
+    @Test
     public void testDifferent_C() throws Exception {
         StringWriter sw = new StringWriter();
         ThreeEqual.impl(new String[] { "9", "9", "0" }, sw);
