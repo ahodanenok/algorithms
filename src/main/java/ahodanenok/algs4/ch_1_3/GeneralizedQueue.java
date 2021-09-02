2796d3d000ec4f041d74784d69c6623a730fb184
Andrei Hodanenok <xoda91@gmail.com>
2021-09-02 21:52:49 +0300

    web, exercise 1.3.3

--- src/main/java/ahodanenok/algs4/ch_1_3/GeneralizedQueue.java
+++ src/main/java/ahodanenok/algs4/ch_1_3/GeneralizedQueue.java
@@ -2,6 +2,9 @@ package ahodanenok.algs4.ch_1_3;
 
 /**
  * Book, exercise 1.3.38
+ *
+ * Web, exercise 1.3.3
+ * https://algs4.cs.princeton.edu/13stacks/
  */
 public interface GeneralizedQueue<T> {
 
