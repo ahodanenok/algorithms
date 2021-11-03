221f735f8999bfefd3767696b535531ed2057f9b
Andrei Hodanenok <xoda91@gmail.com>
2021-11-03 21:16:33 +0300

    book, exercise 1.4.30

--- src/main/java/ahodanenok/algs4/ch_1_3/DequeWithStackSteque.java
+++ src/main/java/ahodanenok/algs4/ch_1_3/DequeWithStackSteque.java
@@ -8,6 +8,8 @@ import java.util.NoSuchElementException;
 /**
  * Web, exercise 1.3.36
  * https://algs4.cs.princeton.edu/13stacks/
+ *
+ * Book, exercise 1.4.30
  */
 public class DequeWithStackSteque<T> implements Deque<T> {
 
