87c64b7d5a7459c91a25bd9471cb99eaaa8d393b
Andrei Hodanenok <xoda91@gmail.com>
2022-01-10 21:13:25 +0300

    book, exercise 3.1.26

--- src/main/java/ahodanenok/algs4/ch_3_1/FrequencyCounter.java
+++ src/main/java/ahodanenok/algs4/ch_3_1/FrequencyCounter.java
@@ -3,19 +3,26 @@ package ahodanenok.algs4.ch_3_1;
 import ahodanenok.algs4.ch_1_3.Stack;
 import edu.princeton.cs.algs4.StdIn;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.FileReader;
+import java.util.*;
 
 public class FrequencyCounter {
 
     // Book, exercise 3.1.8: the
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        String file = args[0];
+
         int minLength;
-        if (args.length > 0) {
-            minLength = Integer.parseInt(args[0]);
+        if (args.length > 1) {
+            minLength = Integer.parseInt(args[1]);
         } else {
             minLength = 1;
+        }
+
+        List<String> countFrequencies = new ArrayList<>();
+        while (!StdIn.isEmpty()) {
+            countFrequencies.add(StdIn.readLine().toLowerCase());
         }
 
         // Book, exercise 3.1.9
@@ -23,22 +30,19 @@ public class FrequencyCounter {
         String lastPut = null;
 
         Map<String, Integer> frequencies = new HashMap<>();
-        while (!StdIn.isEmpty()) {
-            String word = StdIn.readString();
-            if (word.length() >= minLength) {
-                frequencies.merge(word, 1, Integer::sum);
-                lastPut = word;
-            }
+        try (Scanner reader = new Scanner(new FileReader(file))) {
+            while (reader.hasNext()) {
+                String word = reader.next().toLowerCase();
+                if (word.length() >= minLength) {
+                    frequencies.merge(word, 1, Integer::sum);
+                    lastPut = word;
+                }
 
-            processedCount++;
-        }
-
-        int highestFrequency = Integer.MIN_VALUE;
-        for (Integer fq : frequencies.values()) {
-            if (fq > highestFrequency) {
-                highestFrequency = fq;
+                processedCount++;
             }
         }
+
+        int highestFrequency = frequencies.values().stream().mapToInt(Integer::intValue).max().orElse(0);
 
         // Book, exercise 3.1.19
         Stack<String> mostFrequent = new Stack<>();
@@ -48,6 +52,7 @@ public class FrequencyCounter {
             }
         }
 
+        System.out.printf("The most frequent words (%d times):%n", highestFrequency);
         for (String w : mostFrequent) {
             System.out.println(w);
         }
@@ -55,5 +60,17 @@ public class FrequencyCounter {
         System.out.println();
         System.out.println("processed count = " + processedCount);
         System.out.println("last word put = " + lastPut);
+
+        // Book, exercise 3.1.26
+        if (!countFrequencies.isEmpty()) {
+            int width = countFrequencies.stream().mapToInt(String::length).max().orElse(15);
+            countFrequencies.sort(Comparator.comparingInt(w -> frequencies.getOrDefault(w, 0)).reversed());
+
+            System.out.println();
+            System.out.println("Word frequencies:");
+            for (String w : countFrequencies) {
+                System.out.printf("%" + width + "s %d%n", w, frequencies.getOrDefault(w, 0));
+            }
+        }
     }
 }
